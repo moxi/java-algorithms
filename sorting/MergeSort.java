5c7fe7499eb9456611068f046f641581c97ff4c4
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-08-06 08:47:39 -0700

    Adding placeholder for MergeSort
    I’ll finish the implementation later this day.

---
+++ sorting/MergeSort.java
@@ -0,0 +1,18 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+  public static void main(String... args) {
+    int[] input = new int[] {5,2,4,6,1,3};
+    System.out.println( Arrays.toString(mergeSort(input)) );
+  }
+
+  public static int[] mergeSort(int[] input) {
+    int[] sortedArray = null;
+
+    return sortedArray;
+  }
+
+}
