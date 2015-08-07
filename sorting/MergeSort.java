5a10f2ca98a637ec7a359c0774601632ebdb6df8
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-08-07 07:55:06 -0700

    TopDown merge sort

--- sorting/MergeSort.java
+++ sorting/MergeSort.java
@@ -2,6 +2,9 @@ package sorting;
 
 import java.util.Arrays;
 
+/**
+* Based on https://en.wikipedia.org/wiki/Merge_sort#Top-down_implementation_using_lists
+*/
 public class MergeSort {
 
   public static void main(String... args) {
@@ -10,8 +13,47 @@ public class MergeSort {
   }
 
   public static int[] mergeSort(int[] input) {
-    int[] sortedArray = null;
+    if (input.length <= 1) { return input; }
+    int length = input.length;
+    int middle = length / 2;
 
+    int[] left = new int[middle];
+    int[] right = new int[length - middle];
+
+    for (int i = 0; i < middle; ++i) {
+      left[i] = input[i];
+    }
+    for (int i=0, j = middle; j < length; ++i, ++j) {
+      right[i] = input[j];
+    }
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    return merge(left, right);
+  }
+
+  public static int[] merge(int[] left, int[] right) {
+    int[] sortedArray = new int[left.length + right.length];
+    int i = 0, j = 0;
+    while(i + j < sortedArray.length) {
+      if(i < left.length && j < right.length && left[i] <= right[j]) {
+        sortedArray[i+j] = left[i];
+        ++i;
+        continue;
+      } else if (j < right.length) {
+        sortedArray[i+j] = right[j];
+        ++j;
+        continue;
+      }
+      if(i < left.length ) {
+        sortedArray[i+j] = left[i];
+        ++i;
+      } else if (j < right.length) {
+        sortedArray[i+j] = right[j];
+        ++j;
+      }
+    }
     return sortedArray;
   }
 
